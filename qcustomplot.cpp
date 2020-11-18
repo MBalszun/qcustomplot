e03f49c207f011c19a38d5fa4aac40498bdf9837
Michael Balszun <michael.balszun@tum.de>
2020-11-18 12:53:58 +0100

    Update copyright

--- qcustomplot.cpp
+++ qcustomplot.cpp
@@ -2,6 +2,7 @@
 **                                                                        **
 **  QCustomPlot, an easy to use, modern plotting widget for Qt            **
 **  Copyright (C) 2011-2018 Emanuel Eichhammer                            **
+**  Copyright (C) 2020 Michael Balszun                                    **
 **                                                                        **
 **  This program is free software: you can redistribute it and/or modify  **
 **  it under the terms of the GNU General Public License as published by  **
